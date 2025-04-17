525b09211870c4f9f53760e6abdae147d5e702e6
Davide D'Alto <davide@hibernate.org>
2025-04-17 17:47:43 +0200

    Test only with HR

--- src/test/java/org/acme/CounterResourceTest.java
+++ src/test/java/org/acme/CounterResourceTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Supplier;
 
 import jakarta.inject.Inject;
@@ -21,41 +22,16 @@ class CounterResourceTest {
     @Inject
     CounterResource resource;
 
-    @RepeatedTest(10)
-    void should_increase_count_sequentially_using_only_panache() throws Throwable {
+    @RepeatedTest(1)
+    void should_increase_count_sequentially_using_only_hr_session() throws Throwable {
         // given
         final var counter = subscribeAndAwait(() -> resource.createCounter());
         Log.info("Original Counter: " + counter);
 
-        final Supplier<Uni<Counter>> func1 = () -> resource.increaseCounterWithPanacheAndWait(counter.getId());
-        final Supplier<Uni<Counter>> func2 = () -> resource.increaseCounterWithPanache(counter.getId());
-        final var functions = new Supplier[] { func1, func2 };
-        final var counters = new ArrayList<Integer>();
-
-        // when
-        Arrays.stream(functions).parallel().forEach(func -> {
-            try {
-                final var counter2 = (Counter) subscribeAndAwait(func);
-                Log.info("Counter after update: " + counter2);
-                counters.add(counter2.getCount());
-            } catch (final Throwable e) {
-                System.out.println("Error: " + e.getMessage());
-            }
-        });
-
-        // then
-        assertThat(counters).containsExactly(1, 2);
-    }
-
-    @RepeatedTest(10)
-    void should_increase_count_sequentially_using_panache_and_hr_session() throws Throwable {
-        // given
-        final var counter = subscribeAndAwait(() -> resource.createCounter());
-        Log.info("Original Counter: " + counter);
-
-        final Supplier<Uni<Counter>> func1 = () -> resource.increaseCounterWithPanacheAndWait(counter.getId());
-        final Supplier<Uni<Counter>> func2 = () -> resource.increaseCounterWithHrSession(counter.getId());
-        final var functions = new Supplier[] { func1, func2 };
+        final var functions = new Supplier[] {
+                () -> resource.increaseCounterWithHrSessionAndWait(counter.getId()),
+                () -> resource.increaseCounterWithHrSession(counter.getId())
+        };
         final var counters = new ArrayList<Integer>();
 
         // when
